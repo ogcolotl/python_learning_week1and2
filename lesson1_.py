d54271fcc044d4d3374abbda2d9b4071d18a8e79
Colotl-Garista <ocolotl-gar@cps.k12.il.us>
2024-09-03 12:29:03 -0500

    first commit

---
+++ lesson1_.py
@@ -0,0 +1,30 @@
+# this is the print statement
+#print statement allos us to print out a statement
+#comment are not executed by the computer
+#comments are used to help us understand the code
+# print("hello my name is Oscar")
+# print("I am a student at John Hancock College Prep")
+# print("I am learning python")
+#create input for the user\
+
+
+# input allows us to take input from the user
+#ask the user for their age
+#print out the age
+#ask the user for their favorite color
+#print out the favorite color
+#ask the user for their favortie food
+#print out the favortie food
+
+#concatenation is the process of combining strings with
+#variables
+# print("You are" , age, "years old")
+
+name = input("what is your name?")
+print("Hello", name)
+age = input("what is your age?")
+print("You are", age, "years old")
+favcolor = input( "what is your favorite color?")
+print(name, "your favorite color is," , favcolor )
+favfood = input("what is your favorite food?") 
+print("your favorite food is" , favfood)
